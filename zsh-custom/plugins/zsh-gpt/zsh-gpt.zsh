3198764a32839af0ffa938d05eb82fc6c843230c
Chad Provencher <thechad802@gmail.com>
2024-01-07 11:53:47 -0500

    updates zellij layouts

--- zsh-custom/plugins/zsh-gpt/zsh-gpt.zsh
+++ zsh-custom/plugins/zsh-gpt/zsh-gpt.zsh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 export OPENAI_KEY="$(pass personal/open-ai-api)"
+export OPENAI_API_KEY="$(pass personal/chatgpt-nvim)"
 alias chat-ops='chatgpt -m "gpt-4" -i "You are a devops engineer"'
 alias chat-dev='chatgpt -m "gpt-4" -i "You are a software engineer"'
 alias chat-assist='chatgpt -m "gpt-4" -i "You are a personal assistant"'
-
