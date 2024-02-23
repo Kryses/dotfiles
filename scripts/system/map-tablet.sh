01fe89a24fda65554b9bc2c4f0b35ed0ee179d44
Chad Provencher <thechad802@gmail.com>
2024-02-23 18:57:30 -0500

    adds touch to wacom config

--- scripts/system/map-tablet.sh
+++ scripts/system/map-tablet.sh
@@ -2,3 +2,4 @@
 
 xsetwacom set 'Wacom Cintiq Pro 24 Pen stylus' MapToOutput 'HEAD-2'
 xsetwacom set 'Wacom Cintiq Pro 24 Pen eraser' MapToOutput 'HEAD-2'
+xsetwacom set 'Wacom Cintiq Pro 24 Touch Finger touch' MapToOutput 'HEAD-2'
