0dc18f33abf0188bdaed7321b3a5cc9317351aca
Chad Provencher <thechad802@gmail.com>
2024-02-27 07:53:23 -0500

    added project and task tags for filtering

--- scripts/productivity/weekly-report.sh
+++ scripts/productivity/weekly-report.sh
@@ -1,22 +1,20 @@
 #!/usr/bin/zsh
+echo '## Burndown'
 echo '___'
-echo '@code'
-toilet -f big 'Burndown'
+
+echo '```text'
 task burndown.daily 
-echo '@end'
+echo '```'
 echo ''
 echo '___'
 
-echo '___'
-echo '@code'
 toilet -f big 'Tasks'
-echo ''
+echo '___'
+echo '```text'
 echo '___'
 task rc.report.completed.columns=project,description.count,end \
   rc.report.completed.labels=project,description,end \
   completed end.after:$(date -d 'last week' +'%Y-%m-%dT00:00:00') end.before:$(date +'%Y-%m-%dT00:00:00')
-echo '@end'
-echo ''
-echo '___'
+echo '```'
 
 
