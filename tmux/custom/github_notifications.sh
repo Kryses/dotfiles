fb2f870d357f5ace56148f1fde2cfc0643353e5e
Chad Provencher <thechad802@gmail.com>
2024-10-26 13:26:25 -0400

    added github notifications to tmux

---
+++ tmux/custom/github_notifications.sh
@@ -0,0 +1,9 @@
+show_github_notifications() {
+  local index=$1
+  local icon="$(get_tmux_option "@catppuccin_github_notifications_icon" " ")"
+  local color="$(get_tmux_option "@catppuccin_github_notifications_color" "$thm_blue")"
+  local text="$(get_tmux_option "@catppuccin_github_notifications_text" "Test")"
+  local module=$( build_status_module "$index" "$icon" "$color" "$text" )
+
+  echo "$module"
+}
---
+++ tmux/scripts/github_notifications.sh
@@ -0,0 +1,17 @@
+NOTIFICATION_ICON='󰛏'
+PULL_REQUESTS_ICON='󰓂'
+
+all_notifications=$(gh api notifications)
+notifications=$(echo $all_notifications | jq 'keys | length')
+pull_requests=$(echo $all_notifications | jq '[.[] | select(.subject.type == "PullRequest")] | length')
+return_string=""
+echo $pull_requests
+if [ ! $notifications -eq 0 ]; then
+  return_string="$NOTIFICATION_ICON $notifications"
+fi
+
+if [ ! $pull_requests -eq 0 ]; then
+  return_string="$return_string $PULL_REQUESTS_ICON $pull_requests"
+fi
+
+echo -e $return_string
