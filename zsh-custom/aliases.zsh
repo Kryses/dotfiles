106bb61c75227d189f925536c13d45e3724423ab
Chad Provencher <thechad802@gmail.com>
2024-11-03 10:34:12 -0500

    configured nushell

--- zsh-custom/aliases.zsh
+++ zsh-custom/aliases.zsh
@@ -26,6 +26,7 @@ alias kz='zellij kill-all-sessions'
 alias halon-up='nmcli connection up Halon'
 alias halon-down='nmcli connection down Halon'
 alias connect-pipeline="ssh -i $(pass work/hl/aws-ssh)"
+alias notes="cd ~/krys-brain && nvim"
 
 #------------------------------------------------------------
 # Aliases from qtile tutorial
